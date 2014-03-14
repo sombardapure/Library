aa55c25e911dcaf74d008069a99807bc7d807873
sombardapure <sombardapure@yahoo.com>
2014-03-14 19:18:26 +0530

    Added logging features

--- Library/src/main/java/com/barclays/library/service/LibraryService.java
+++ Library/src/main/java/com/barclays/library/service/LibraryService.java
@@ -2,29 +2,36 @@ package com.barclays.library.service;
 
 import java.util.Set;
 
+import org.apache.log4j.Logger;
+
 import com.barclays.library.bean.Book;
 import com.barclays.library.bean.Member;
 
 public class LibraryService {
 
-	private static LibraryService service = new LibraryService();
+	private static final Logger logger = Logger.getLogger(LibraryService.class);
+	
+	private static final LibraryService service = new LibraryService();
 	
 	private LibraryService(){
 	}
 	
-	public LibraryService getInstance(){
+	public static LibraryService getInstance(){
 		return service;
 	}
 	
 	public Set<Book> findBooksByAuthor(String author){
+		logger.info("Executing findBooksByAuthor");
 		return null;
 	}
 	
 	public Set<Book> findBooksByTitle(String title){
+		logger.info("Executing findBooksByTitle");
 		return null;
 	}
 	
 	public boolean addMember(Member member){
+		logger.info("Executing add member");
 		return false;
 	}
 	
--- Library/src/test/java/com/barclays/library/LibraryServiceTest.java
+++ Library/src/test/java/com/barclays/library/LibraryServiceTest.java
@@ -1,12 +1,21 @@
 package com.barclays.library;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.barclays.library.service.LibraryService;
 
 public class LibraryServiceTest {
 
+	private static LibraryService libraryService;
+	
+	@BeforeClass
+	public static void setUp(){
+		libraryService = LibraryService.getInstance();
+	}
 	@Test
 	public void findBooksByAuthor(){
-		
+		libraryService.findBooksByAuthor("");
 	}
 	
 }
