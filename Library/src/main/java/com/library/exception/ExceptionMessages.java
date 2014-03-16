eab4b47bdd99b462f7af460980644f1c0c372778
sombardapure <sombardapure@yahoo.com>
2014-03-16 13:22:05 +0530

    Added exception messages

---
+++ Library/src/main/java/com/library/exception/ExceptionMessages.java
@@ -0,0 +1,12 @@
+package com.library.exception;
+
+/**
+ * Class to maintain exception messages used in the application
+ * @author Somanath
+ *
+ */
+public class ExceptionMessages {
+	
+	public static final String MAX_NO_OF_BOOKS = "At most 3 books can be issued";
+	public static final String REF_BOOK_CAN_NOT_BE_ISSUED = "Reference book can not be issued";
+}
