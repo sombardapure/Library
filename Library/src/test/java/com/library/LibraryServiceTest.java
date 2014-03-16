dd47bb0162524e11c6646c6d4ac976089b4e8d46
sombardapure <sombardapure@yahoo.com>
2014-03-16 12:41:52 +0530

    Added test case for findBookByAuthor

--- Library/src/test/java/com/library/LibraryServiceTest.java
+++ Library/src/test/java/com/library/LibraryServiceTest.java
@@ -1,13 +1,17 @@
 package com.library;
 
 import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotNull;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.library.bean.Book;
+import com.library.enums.BookType;
 import com.library.service.LibraryService;
 
 /**
@@ -33,6 +37,18 @@ public class LibraryServiceTest {
 	public void findBooksByAuthorAsBlank(){
 		Set<Book> actualResult = libraryService.findBooksByAuthor("");
 		assertEquals(null, actualResult);
+	}
+	
+	@Test
+	public void findBooksByAuthor(){
+		Set<Book> actualResult = libraryService.findBooksByAuthor("Sierra");
+		
+		Set<Book> expectedResult = new HashSet<Book>();
+		expectedResult.add(new Book(1L, "Kathy Sierra", "Head First Java", BookType.BORROWABLE));
+		expectedResult.add(new Book(6L, "Kathy Sierra", "Head First Java", BookType.BORROWABLE));
+		
+		assertNotNull(actualResult);
+		assertArrayEquals(actualResult.toArray(), expectedResult.toArray());
 	}
 	
 }
